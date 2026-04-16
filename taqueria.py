403ffef916a43fcd07315a2e833df9582033cb97
L <1@gg.com>
2026-04-16 19:59:24 +0200

    Nutrition Facts

---
+++ taqueria.py
@@ -0,0 +1,44 @@
+def main():
+    print(order())
+
+def order():
+    menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00 }
+
+    total = 0
+
+    while True:
+        item = ask_user()
+
+        if (item == "End"):
+            break
+
+        try:
+            if item in menu:
+                total += menu[item]
+                print("Total: " "$" + f"{total:.2f}")
+            
+            
+        except KeyError:
+            pass
+
+        except ValueError:
+            pass
+
+        except EOFError:
+            pass
+
+
+
+def ask_user():
+    return input("Item: ").strip().title()
+
+main()
